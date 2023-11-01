81db91e15f448d24b37911a9838d60de7b4788be
Damian Galeano <dj.damian.b@hotmail.com>
2023-11-01 09:10:59 +0100

    Build Front

---
+++ front/index.js
@@ -0,0 +1,24 @@
+const imageForm = document.querySelector('#imageForm');
+const imageInput = document.querySelector('#imageInput');
+
+
+
+imageForm.addEventListener("submit", async event => {
+    event.preventDefault()
+    const file = imageInput.files[0]
+
+
+
+    // GET SECURE URL
+
+
+
+
+    // POST IMAGE TO S3
+
+
+
+
+
+    // POST REQUEST
+})
